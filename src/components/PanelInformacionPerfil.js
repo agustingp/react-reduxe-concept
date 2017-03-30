0ab306ff0fe466ee52f8a5de2661956cf835a2b3
apereira <apereira@hexacta.com>
2017-03-30 12:43:25 -0300

    correccion errores

--- src/components/PanelInformacionPerfil.js
+++ src/components/PanelInformacionPerfil.js
@@ -5,27 +5,27 @@ export default ({ infoPerfil }) => {
     <div className="panel-info-perfil">
       <div className="campo-panel-info-perfil">
         <label htmlFor="nombre"> Nombre de la ONG </label>
-        <input type="text" value={ infoPerfil.name } size="30" />
+        <input type="text" defaultValue={ infoPerfil.name } size="30" />
       </div>
       <div className="campo-panel-info-perfil">
         <label htmlFor="nombre"> Tipo </label>
-        <input type="text" value={ infoPerfil.type } size="30" />
+        <input type="text" defaultValue={ infoPerfil.type } size="30" />
       </div>
       <div className="campo-panel-info-perfil">
         <label htmlFor="nombre"> Teléfono </label>
-        <input type="text" value={ infoPerfil.phone } size="30" />
+        <input type="text" defaultValue={ infoPerfil.phone } size="30" />
       </div>
       <div className="campo-panel-info-perfil">
         <label htmlFor="nombre"> Email </label>
-        <input type="text" value={ infoPerfil.email } size="30" />
+        <input type="text" defaultValue={ infoPerfil.email } size="30" />
       </div>
       <div className="campo-panel-info-perfil">
         <label htmlFor="nombre"> Website </label>
-        <input type="text" value={ infoPerfil.website } size="30" />
+        <input type="text" defaultValue={ infoPerfil.website } size="30" />
       </div>
       <div className="campo-panel-info-perfil">
         <label htmlFor="nombre"> Descripcion </label>
-        <textarea value={ infoPerfil.descrription } size="30" />
+        <textarea defaultValue={ infoPerfil.description } size="30" />
       </div>
     </div>
 )};
--- src/components/SignInUp.js
+++ src/components/SignInUp.js
@@ -4,7 +4,10 @@ import PanelPerfil from './PanelInformacionPerfil';
 
 export default ({ ong, OnSignInUp }) => {
   return (
-  <form onSubmit={ OnSignInUp }>
+  <form onSubmit={(e) => {
+         e.preventDefault();
+         OnSignInUp(ong);
+       }}>
     <PanelPerfil infoPerfil={ ong } />
     <input type="submit" value="Sign-In" />
   </form>
